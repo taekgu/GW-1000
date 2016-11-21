8c95cb512a6a6278693175ecc0f5be7129f14894
Jinwook <jjw543@naver.com>
2016-11-21 17:14:54 +0900

    2016.11.21 17:14 / 시작, 일시정지, 정지 send

--- app/src/main/java/com/sinest/gw_1000/mode/Fragment_working.java
+++ app/src/main/java/com/sinest/gw_1000/mode/Fragment_working.java
@@ -11,6 +11,9 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.sinest.gw_1000.R;
+import com.sinest.gw_1000.communication.Communicator;
+import com.sinest.gw_1000.management.Application_broadcast;
+import com.sinest.gw_1000.management.Application_communicator;
 
 /**
  * Created by Jinwook on 2016-11-21.
@@ -75,21 +78,26 @@ public class Fragment_working extends Fragment {
             }
             else if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
 
+                Communicator communicator = Application_communicator.getCommunicator();
                 switch (id) {
                     case R.id.button_play:
 
                         button_clicked.setBackgroundResource(R.drawable.button_play_off);
+                        communicator.set_tx(1, (byte)0x01);
                         break;
                     case R.id.button_pause:
 
                         button_clicked.setBackgroundResource(R.drawable.button_pause_off);
+                        communicator.set_tx(1, (byte)0x02);
                         break;
                     case R.id.button_stop:
 
                         button_clicked.setBackgroundResource(R.drawable.button_stop_off);
+                        communicator.set_tx(1, (byte)0x00);
                         activity_waiting.changeFragment_waiting();
                         break;
                 }
+                communicator.send(communicator.get_tx());
             }
             return true;
         }
