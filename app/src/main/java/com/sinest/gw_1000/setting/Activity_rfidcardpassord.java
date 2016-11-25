b1fe98b4bbcde7c1a1dd82a95111199a03f31a84
sb <taekgu04@naver.com>
2016-11-25 13:40:52 +0900

    2016.11.25 01:40 rfid_ps
    password

--- app/src/main/java/com/sinest/gw_1000/setting/Activity_rfidcardpassord.java
+++ app/src/main/java/com/sinest/gw_1000/setting/Activity_rfidcardpassord.java
@@ -2,6 +2,7 @@ package com.sinest.gw_1000.setting;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -26,10 +27,14 @@ public class Activity_rfidcardpassord extends Activity {
 
     TextView rfid_p;
     Intent intent_rfid;
+    Intent intent_pscheck;
 
-    String s_buf;
-    int int_buf;
+    char[] s_buf = {' ',' ',' ',' '};
+    String ps = "";
+    int int_buf = 0;
     int int_c = 0;
+    String mater_ps = "5866";
+    String password = "0000";
 
     Intent check;
 
@@ -58,10 +63,15 @@ public class Activity_rfidcardpassord extends Activity {
         rfid_password_e = (Button) findViewById(R.id.rfid_password_e);
         rfid_password_b = (Button) findViewById(R.id.rfid_password_b);
 
+        Typeface tf = Typeface.createFromAsset(getAssets(), "fonts/digital.ttf");
         rfid_p = (TextView) findViewById(R.id.rfid_p);
+        rfid_p.setTypeface(tf);
+
 
         intent_rfid = new Intent(this, Activity_rfid.class);
         intent_rfid.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+        intent_pscheck = new Intent(this, Activity_rfidpassword_check.class);
 
         check = this.getIntent();
 
@@ -70,196 +80,76 @@ public class Activity_rfidcardpassord extends Activity {
                 switch (v.getId()) {
                     case R.id.rfid_password_1:
                         //
-                        if(int_c >= 4)
-                        {
-                            s_buf = (String)rfid_p.getText();
-                            int_buf = Integer.parseInt(s_buf);
-                            int_buf = (int_buf%1000)*10 + 1;
-                            s_buf = Integer.toString(int_buf);
-                            rfid_p.setText(s_buf);
-                        }else
-                        {
-                            s_buf = (String)rfid_p.getText();
-                            int_buf = Integer.parseInt(s_buf)*10 + 1;
-                            s_buf = Integer.toString(int_buf);
-                            rfid_p.setText(s_buf);
-                            int_c++;
-                        }
+                        addps('1');
+                        ps = ""+s_buf[0]+s_buf[1]+s_buf[2]+s_buf[3];
+                        Log.v("test","ps : "+ps);
+                        rfid_p.setText(ps);
                         break;
                     case R.id.rfid_password_2:
                         //
-                        if(int_c >= 4)
-                        {
-                            s_buf = (String)rfid_p.getText();
-                            int_buf = Integer.parseInt(s_buf);
-                            int_buf = (int_buf%1000)*10 + 2;
-                            s_buf = Integer.toString(int_buf);
-                            rfid_p.setText(s_buf);
-                        }else
-                        {
-                            s_buf = (String)rfid_p.getText();
-                            int_buf = Integer.parseInt(s_buf)*10 + 2;
-                            s_buf = Integer.toString(int_buf);
-                            rfid_p.setText(s_buf);
-                            int_c++;
-                        }
+                        addps('2');
+                        ps = ""+s_buf[0]+s_buf[1]+s_buf[2]+s_buf[3];
+                        rfid_p.setText(ps);
                         break;
                     case R.id.rfid_password_3:
                         //
-                        if(int_c >= 4)
-                        {
-                            s_buf = (String)rfid_p.getText();
-                            int_buf = Integer.parseInt(s_buf);
-                            int_buf = (int_buf%1000)*10 + 3;
-                            s_buf = Integer.toString(int_buf);
-                            rfid_p.setText(s_buf);
-                        }else
-                        {
-                            s_buf = (String)rfid_p.getText();
-                            int_buf = Integer.parseInt(s_buf)*10 + 3;
-                            s_buf = Integer.toString(int_buf);
-                            rfid_p.setText(s_buf);
-                            int_c++;
-                        }
+                        addps('3');
+                        ps = ""+s_buf[0]+s_buf[1]+s_buf[2]+s_buf[3];
+                        rfid_p.setText(ps);
                         break;
                     case R.id.rfid_password_4:
                         //
-                        if(int_c >= 4)
-                        {
-                            s_buf = (String)rfid_p.getText();
-                            int_buf = Integer.parseInt(s_buf);
-                            int_buf = (int_buf%1000)*10 + 4;
-                            s_buf = Integer.toString(int_buf);
-                            rfid_p.setText(s_buf);
-                        }else
-                        {
-                            s_buf = (String)rfid_p.getText();
-                            int_buf = Integer.parseInt(s_buf)*10 + 4;
-                            s_buf = Integer.toString(int_buf);
-                            rfid_p.setText(s_buf);
-                            int_c++;
-                        }
+                        addps('4');
+                        ps = ""+s_buf[0]+s_buf[1]+s_buf[2]+s_buf[3];
+                        rfid_p.setText(ps);
                         break;
                     case R.id.rfid_password_5:
                         //
-                        if(int_c >= 4)
-                        {
-                            s_buf = (String)rfid_p.getText();
-                            int_buf = Integer.parseInt(s_buf);
-                            int_buf = (int_buf%1000)*10 + 5;
-                            s_buf = Integer.toString(int_buf);
-                            rfid_p.setText(s_buf);
-                        }else
-                        {
-                            s_buf = (String)rfid_p.getText();
-                            int_buf = Integer.parseInt(s_buf)*10 + 5;
-                            s_buf = Integer.toString(int_buf);
-                            rfid_p.setText(s_buf);
-                            int_c++;
-                        }
+                        addps('5');
+                        ps = ""+s_buf[0]+s_buf[1]+s_buf[2]+s_buf[3];
+                        rfid_p.setText(ps);
                         break;
                     case R.id.rfid_password_6:
                         //
-                        if(int_c >= 4)
-                        {
-                            s_buf = (String)rfid_p.getText();
-                            int_buf = Integer.parseInt(s_buf);
-                            int_buf = (int_buf%1000)*10 + 6;
-                            s_buf = Integer.toString(int_buf);
-                            rfid_p.setText(s_buf);
-                        }else
-                        {
-                            s_buf = (String)rfid_p.getText();
-                            int_buf = Integer.parseInt(s_buf)*10 + 6;
-                            s_buf = Integer.toString(int_buf);
-                            rfid_p.setText(s_buf);
-                            int_c++;
-                        }
+                        addps('6');
+                        ps = ""+s_buf[0]+s_buf[1]+s_buf[2]+s_buf[3];
+                        rfid_p.setText(ps);
                         break;
                     case R.id.rfid_password_7:
                         //
-                        if(int_c >= 4)
-                        {
-                            s_buf = (String)rfid_p.getText();
-                            int_buf = Integer.parseInt(s_buf);
-                            int_buf = (int_buf%1000)*10 + 7;
-                            s_buf = Integer.toString(int_buf);
-                            rfid_p.setText(s_buf);
-                        }else
-                        {
-                            s_buf = (String)rfid_p.getText();
-                            int_buf = Integer.parseInt(s_buf)*10 + 7;
-                            s_buf = Integer.toString(int_buf);
-                            rfid_p.setText(s_buf);
-                            int_c++;
-                        }
+                        addps('7');
+                        ps = ""+s_buf[0]+s_buf[1]+s_buf[2]+s_buf[3];
+                        rfid_p.setText(ps);
                         break;
                     case R.id.rfid_password_8:
                         //
-                        if(int_c >= 4)
-                        {
-                            s_buf = (String)rfid_p.getText();
-                            int_buf = Integer.parseInt(s_buf);
-                            int_buf = (int_buf%1000)*10 + 8;
-                            s_buf = Integer.toString(int_buf);
-                            rfid_p.setText(s_buf);
-                        }else
-                        {
-                            s_buf = (String)rfid_p.getText();
-                            int_buf = Integer.parseInt(s_buf)*10 + 8;
-                            s_buf = Integer.toString(int_buf);
-                            rfid_p.setText(s_buf);
-                            int_c++;
-                        }
+                        addps('8');
+                        ps = ""+s_buf[0]+s_buf[1]+s_buf[2]+s_buf[3];
+                        rfid_p.setText(ps);
                         break;
                     case R.id.rfid_password_9:
                         //
-                        if(int_c >= 4)
-                        {
-                            s_buf = (String)rfid_p.getText();
-                            int_buf = Integer.parseInt(s_buf);
-                            int_buf = (int_buf%1000)*10 + 9;
-                            s_buf = Integer.toString(int_buf);
-                            rfid_p.setText(s_buf);
-                        }else
-                        {
-                            s_buf = (String)rfid_p.getText();
-                            int_buf = Integer.parseInt(s_buf)*10 + 9;
-                            s_buf = Integer.toString(int_buf);
-                            rfid_p.setText(s_buf);
-                            int_c++;
-                        }
+                        addps('9');
+                        ps = ""+s_buf[0]+s_buf[1]+s_buf[2]+s_buf[3];
+                        rfid_p.setText(ps);
                         break;
                     case R.id.rfid_password_0:
                         //
-                        if(int_c >= 4)
-                        {
-                            s_buf = (String)rfid_p.getText();
-                            int_buf = Integer.parseInt(s_buf);
-                            int_buf = (int_buf%1000)*10 + 0;
-                            s_buf = Integer.toString(int_buf);
-                            rfid_p.setText(s_buf);
-                        }else
-                        {
-                            s_buf = (String)rfid_p.getText();
-                            int_buf = Integer.parseInt(s_buf)*10 + 0;
-                            s_buf = Integer.toString(int_buf);
-                            rfid_p.setText(s_buf);
-                            int_c++;
-                        }
+                        addps('0');
+                        ps = ""+s_buf[0]+s_buf[1]+s_buf[2]+s_buf[3];
+                        rfid_p.setText(ps);
                         break;
                     //-----------------------------------------------
                     case R.id.rfid_password_d:
                         //
-                        s_buf = (String)rfid_p.getText();
-                        int_buf = Integer.parseInt(s_buf);
-                        int_buf = int_buf/10;
-                        s_buf = Integer.toString(int_buf);
-                        rfid_p.setText(s_buf);
+                        delps();
+                        ps = ""+s_buf[0]+s_buf[1]+s_buf[2]+s_buf[3];
+                        rfid_p.setText(ps);
                         break;
                     case R.id.rfid_password_c:
                         //
-
+                        intent_pscheck.putExtra("pr_pass",password);
+                        startActivityForResult(intent_pscheck,17);
                         break;
                     case R.id.rfid_password_b:
                         //
@@ -270,8 +160,16 @@ public class Activity_rfidcardpassord extends Activity {
                         break;
                     case R.id.rfid_password_e:
                         //
-                        startActivity(intent_rfid);
-                        finish();
+                        if(ps.equals(mater_ps)){
+                            password = "0000";
+                        }
+                        else if(ps.equals(password)){
+                            startActivity(intent_rfid);
+                            finish();
+                        }
+                        else{
+                            rfid_p.setText("!"+s_buf[0]+s_buf[1]+s_buf[2]+s_buf[3]);
+                        }
                         break;
                 }
                 //Toast.makeText(Activity_rfidcardpassord.this, s_buf, Toast.LENGTH_SHORT).show();
@@ -293,5 +191,52 @@ public class Activity_rfidcardpassord extends Activity {
         rfid_password_e.setOnClickListener(listener);
         rfid_password_b.setOnClickListener(listener);
 
+    }
+
+    void addps(char num){
+        if(int_c == 0)
+        {
+            s_buf[0] = num;
+            int_c++;
+        }else if(int_c == 1)
+        {
+            s_buf[1] = num;
+            int_c++;
+        }else if(int_c == 2)
+        {
+            s_buf[2] = num;
+            int_c++;
+        }else if(int_c == 3)
+        {
+            s_buf[3] = num;
+            int_c++;
+        }else if(int_c == 4){
+            s_buf[0] = s_buf[1];
+            s_buf[1] = s_buf[2];
+            s_buf[2] = s_buf[3];
+            s_buf[3] = num;
+        }
+    }
+
+    void delps(){
+        if(int_c == 0)
+        {
+            s_buf[0] = ' ';
+        }else if(int_c == 1)
+        {
+            s_buf[0] = ' ';
+            int_c--;
+        }else if(int_c == 2)
+        {
+            s_buf[1] = ' ';
+            int_c--;
+        }else if(int_c == 3)
+        {
+            s_buf[2] = ' ';
+            int_c--;
+        }else if(int_c == 4){
+            s_buf[3] = ' ';
+            int_c--;
+        }
     }
 }
