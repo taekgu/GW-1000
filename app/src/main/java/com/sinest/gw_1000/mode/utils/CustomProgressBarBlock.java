2b3ee956c513552f82960c0fdfb2a66b801dfda0
byeoneon <kp8506@naver.com>
2017-01-31 19:09:33 +0900

    2017.01.31 working 노즐위치 프로그래스바

--- app/src/main/java/com/sinest/gw_1000/mode/utils/CustomProgressBarBlock.java
+++ app/src/main/java/com/sinest/gw_1000/mode/utils/CustomProgressBarBlock.java
@@ -2,12 +2,10 @@ package com.sinest.gw_1000.mode.utils;
 
 import android.content.Context;
 import android.content.res.Resources;
-import android.os.Build;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewTreeObserver;
 import android.widget.RelativeLayout;
 
 import com.sinest.gw_1000.R;
@@ -16,9 +14,7 @@ public class CustomProgressBarBlock extends RelativeLayout {
     private RelativeLayout relFilter;
     private int currentWidth;
     private View viewParent;
-    private View mainView;
     private Context context;
-    private RelativeLayout relativeLayout;
 
     public CustomProgressBarBlock(Context context) {
         super(context);
@@ -39,46 +35,15 @@ public class CustomProgressBarBlock extends RelativeLayout {
     }
     private void initialize(Context context) {
         inflate(context, R.layout.layout_custom_progress_bar_block, this);
-        relativeLayout = (RelativeLayout) findViewById(R.id.custom_progress_bar_block);
         viewParent = findViewById(R.id.progress_filter_parent_block);
         relFilter = (RelativeLayout) findViewById(R.id.rel_progress_block);
-        mainView = findViewById(R.id.progress_main_view_block);
-    }
-    private void init() {
-        //ViewCompat.setElevation(tvFilterMin, 14f);
-        final ViewTreeObserver viewTreeObserver = relativeLayout.getViewTreeObserver();
-        viewTreeObserver.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
-            public void onGlobalLayout() {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-                    viewParent.getViewTreeObserver().removeOnGlobalLayoutListener(this);
-                } else {
-                    viewParent.getViewTreeObserver().removeGlobalOnLayoutListener(this);
-                }
-                currentWidth = relFilter.getWidth();
-            }
-        });
+        currentWidth = relFilter.getWidth();
     }
     public void setProgress(final int loc) {
-        viewParent.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
-            @Override
-            public void onGlobalLayout() {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-                    viewParent.getViewTreeObserver().removeOnGlobalLayoutListener(this);
-                } else {
-                    viewParent.getViewTreeObserver().removeGlobalOnLayoutListener(this);
-                }
-                currentWidth = ((loc * (viewParent.getWidth()) / 14));
-                if(loc==1)
-                    mainView.setBackgroundResource(R.drawable.bar_min);
-                else if(loc==14)
-                    mainView.setBackgroundResource(R.drawable.bar_max);
-                else
-                    mainView.setBackgroundResource(R.drawable.bar_base_horizontal);
-                ViewGroup.LayoutParams layoutParams = relFilter.getLayoutParams();
-                layoutParams.width = currentWidth;
-                relFilter.setLayoutParams(layoutParams);
-            }
-        });
+        currentWidth = ((loc * (viewParent.getWidth()) / 14));
+        ViewGroup.LayoutParams layoutParams = relFilter.getLayoutParams();
+        layoutParams.width = currentWidth;
+        relFilter.setLayoutParams(layoutParams);
     }
 }
 
