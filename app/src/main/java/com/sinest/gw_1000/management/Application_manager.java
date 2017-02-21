798564b84904a0bdcc4e993184a2d86ceff05794
sb <taekgu04@naver.com>
2017-02-21 15:07:03 +0900

    sb - 중국어 이미지 수정 2
    inverter , door

--- app/src/main/java/com/sinest/gw_1000/management/Application_manager.java
+++ app/src/main/java/com/sinest/gw_1000/management/Application_manager.java
@@ -232,7 +232,7 @@ public class Application_manager extends Application {
     public static int[] emotion_off = {R.drawable.emotion_off, R.drawable.emotion_off_ch};
     public static int[] language_ch = {R.drawable.language_ch, R.drawable.language_ch_ch};
     public static int[] setting_back_image = {R.drawable.setting_back_image, R.drawable.setting_back_image_ch};
-    public static int[] inverter_0 = {R.drawable.inverter_0, R.drawable.inverter_0};
+    public static int[] inverter_0 = {R.drawable.inverter_0, R.drawable.inverter_0_ch};
     public static int[] inverter_50 = {R.drawable.inverter_50, R.drawable.inverter_50_ch};
     public static int[] inverter_100 = {R.drawable.inverter_100, R.drawable.inverter_100_ch};
 
